99462e8f07bfe4cbd30f553d1cdc027df8dbb338
camycamily <camily.goncalves.freitas@escola.pr.gov.br>
2025-05-27 20:55:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Jogos Vorazes/Livros',
+    'Qual tributo masculino do Distrito 12 participa dos Jogos Vorazes junto com Katniss no primeiro livro?',
+    'Peeta Mellark.'
+)
+
+criaCartao(
+    'Percy Jackson/Livros',
+    'Qual é o nome do sátiro que é amigo de Percy?',
+    'Grover Underwood.'
+)
+
+criaCartao(
+    'Crepúsculo/Livros',
+    'Como se chama a cidade onde Bella Swan vai morar?',
+    'Forks.'
+)
+
+criaCartao(
+    'Divergente/Livros',
+    'Que facção Tris escolhe ao sair da Abnegação?',
+    'Audácia.'
+)
+
+criaCartao(
+    'Coraline/Livros',
+    'Qual o nome dos vizinhos de Coraline?',
+    'O Sr. Bobinsky, a Sra. Spink e a Sra. Forcible.'
+)
+
+criaCartao(
+    'O Senhor Dos Anéis/Livros',
+    'Quem era o portador do Anel antes de Frodo??',
+    'Bilbo Bolseiro.'
+)
